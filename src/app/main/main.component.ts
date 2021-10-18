86d522217917454affc59436db280ef7257094a2
avrik <meavrik@gmail.com>
2021-10-18 16:37:17 +0300

    update changes

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,25 +1,50 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { Chart } from "chart.js";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"],
 })
 export class MainComponent implements OnInit {
-  currentFocus
-  @ViewChild('inp', { static: true }) inp: ElementRef;
+  currentFocus;
+  @ViewChild("chart") chartRef: ElementRef;
+  @ViewChild("inp", { static: true }) inp: ElementRef;
 
-  completeArr = ['About', 'contact', 'history', 'what', 'who', 'why', 'story', 'portfolio']
+  completeArr = [
+    "About",
+    "contact",
+    "history",
+    "what",
+    "who",
+    "why",
+    "story",
+    "portfolio",
+  ];
   suggestionsArr = [];
-  searchStr = '';
-  constructor() { }
+  searchStr = "";
+  data
+  constructor() {}
 
   ngOnInit() {
+    var ctx = document.getElementById("myChart");
+    setTimeout(() => {
+      this.data = {
+        datasets: [
+          {
+            data: [10, 20, 30],
+          },
+        ],
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: ["Red", "Yellow", "Blue"],
+      };
+    }, 10);
   }
 
   onInputChange(event) {
-
-    this.suggestionsArr = this.searchStr ? this.completeArr.filter(a => a.indexOf(this.searchStr) !== -1) : [];
+    this.suggestionsArr = this.searchStr
+      ? this.completeArr.filter((a) => a.indexOf(this.searchStr) !== -1)
+      : [];
   }
-
 }
